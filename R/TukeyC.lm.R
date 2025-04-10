7d48154e9ac6c9818d099f3c704ef300b9444d3f
Ivan Bezerra Allaman <ivanalaman@gmail.com>
2025-04-10 08:50:08 +0000

    version 1.3-43

--- R/TukeyC.lm.R
+++ R/TukeyC.lm.R
@@ -167,7 +167,7 @@ TukeyC.lm <- function(x,
                                            effect = which,
                                            level = 1 - sig.level))
 
-  aux_mt1 <- aux_mt$coef[,1]
+  aux_mt1 <- with(aux_mt,estimate)
 
   aux_mt2 <- data.frame(means = aux_mt1,
                         reps = reps)
--- R/TukeyC.lmerMod.R
+++ R/TukeyC.lmerMod.R
@@ -184,7 +184,7 @@ TukeyC.lmerMod <- function(x,
                                            effect = which,
                                            level = 1 - sig.level))
 
-  aux_mt1 <- aux_mt$coef[,1]
+  aux_mt1 <- with(aux_mt,estimate)
 
   aux_mt2 <- data.frame(means = aux_mt1,
                         reps = reps)
